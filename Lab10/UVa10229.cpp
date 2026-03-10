9c4f609b8d27f8c139c8ac3bb9d2e88517b6640f
Achii11 <684244145@mail.pbru.ac.th>
2026-03-10 14:16:36 +0700

    Update UVa10229.cpp

--- Lab10/UVa10229.cpp
+++ Lab10/UVa10229.cpp
@@ -1,57 +1,59 @@
+// Modular Fibonacci
+
 #include <bits/stdc++.h>
 using namespace std;
 
 typedef long long ll;
 
-const int MAX_N = 100010;
-const int p = 1e9+7;                             // p is a prime > MAX_N
+ll MOD;
 
-ll mod(ll a, int m) {                            // returns a (mod m)
-  return ((a%m) + m) % m;                        // ensure positive answer
+const int MAX_N = 2;                             // 2x2 for Fib matrix
+
+struct Matrix { ll mat[MAX_N][MAX_N]; };         // we return a 2D array
+
+ll mod(ll a, ll m) { return ((a%m)+m) % m; }     // ensure positive answer
+
+Matrix matMul(Matrix a, Matrix b) {              // normally O(n^3)
+  Matrix ans;                                    // but O(1) as n = 2
+  for (int i = 0; i < MAX_N; ++i)
+    for (int j = 0; j < MAX_N; ++j)
+      ans.mat[i][j] = 0;
+  for (int i = 0; i < MAX_N; ++i)
+    for (int k = 0; k < MAX_N; ++k) {
+      if (a.mat[i][k] == 0) continue;            // optimization
+      for (int j = 0; j < MAX_N; ++j) {
+        ans.mat[i][j] += mod(a.mat[i][k], MOD) * mod(b.mat[k][j], MOD);
+        ans.mat[i][j] = mod(ans.mat[i][j], MOD); // modular arithmetic
+      }
+    }
+  return ans;
 }
 
-ll modPow(ll b, int p, int m) {                  // assume 0 <= b < m
-  if (p == 0) return 1;
-  ll ans = modPow(b, p/2, m);                    // this is O(log p)
-  ans = mod(ans*ans, m);                         // double it first
-  if (p&1) ans = mod(ans*b, m);                  // *b if p is odd
-  return ans;                                    // ans always in [0..m-1]
+Matrix matPow(Matrix base, int p) {              // normally O(n^3 log p)
+  Matrix ans;                                    // but O(log p) as n = 2
+  for (int i = 0; i < MAX_N; ++i)
+    for (int j = 0; j < MAX_N; ++j)
+      ans.mat[i][j] = (i == j);                  // prepare identity matrix
+  while (p) {                                    // iterative D&C version
+    if (p&1)                                     // check if p is odd
+      ans = matMul(ans, base);                   // update ans
+    base = matMul(base, base);                   // square the base
+    p >>= 1;                                     // divide p by 2
+  }
+  return ans;
 }
-
-ll inv(ll a) {                                   // Fermat's little theorem
-  return modPow(a, p-2, p);                      // modPow in Section 5.8
-}                                                // that runs in O(log p)
-
-ll fact[MAX_N], invFact[MAX_N];
-
-ll C(int n, int k) {                             // O(log p)
-  if (n < k) return 0;                           // clearly
-  return (((fact[n] * inv(fact[k])) % p) * inv(fact[n-k])) % p;
-  // return (((fact[n] * invFact[k]) % p) * invFact[n-k]) % p; // O(1)
-}
-
-ll Fib[MAX_N], Cat[MAX_N];
 
 int main() {
-  Fib[0] = 0;                                    // the first
-  Fib[1] = 1;                                    // two terms of Fibonacci
-  for (int i = 2; i < MAX_N; ++i)                // O(MAX_N) pre-processing
-    Fib[i] = (Fib[i-1] + Fib[i-2]) % p;          // Fib[i] in [0..p-1]
-  cout << Fib[100000] << "\n";                   // the answer is 911435502
-
-  fact[0] = 1;
-  for (int i = 1; i < MAX_N; ++i)                // O(MAX_N) pre-processing
-    fact[i] = (fact[i-1]*i)% p;                  // fact[i] in [0..p-1]
-
-  // invFact[MAX_N-1] = inv(fact[MAX_N-1]);         // one O(log p)
-  // for (int i = MAX_N-2; i >= 0; --i)             // O(MAX_N) pre-processing
-  //   invFact[i] = (invFact[i+1]*(i+1)) % p;
-  cout << C(100000, 50000) << "\n";              // the answer is 149033233
-
-  Cat[0] = 1;
-  for (int n = 0; n < MAX_N-1; ++n)              // O(MAX_N * log p)
-    Cat[n+1] = ((4*n+2)%p * Cat[n]%p * inv(n+2)) % p;
-  cout << Cat[100000] << "\n";                   // the answer is 945729344
-
+  int n, m;
+  while (scanf("%d %d", &n, &m) == 2) {
+    Matrix ans;                                  // Fibonaccci matrix
+    ans.mat[0][0] = 1;  ans.mat[0][1] = 1;
+    ans.mat[1][0] = 1;  ans.mat[1][1] = 0;
+    MOD = 1;
+    for (int i = 0; i < m; ++i)                  // set MOD = 2^m
+      MOD *= 2;
+    ans = matPow(ans, n);                        // O(log n) 
+    printf("%lld\n", ans.mat[0][1]);             // this if fib(n) % MOD
+  }
   return 0;
 }
